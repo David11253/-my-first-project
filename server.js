bf69bd1df52879f0419006c282ae69c65f9cc752
David11253 <minerusiapiter58@gmail.com>
2025-01-07 21:00:09 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Сервируем статические файлы
+app.use(express.static(path.join(__dirname)));
+
+// Обработка корневого маршрута
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
